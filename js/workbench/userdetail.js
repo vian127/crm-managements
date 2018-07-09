97bfcded8f28fbd3a874d1b9770ffb5be53f65a5
Binlu <826202368@qq.com>
2018-07-09 18:17:42 +0800

    客户详情页修改

--- js/workbench/userdetail.js
+++ js/workbench/userdetail.js
@@ -30,8 +30,47 @@ require(['vue','ztree-excheck'], function (Vue) {
           })
       });
 
+    var def={
+      trees:{
+        hangye:[
+          { id:1, pId:0, name:'随意勾选 1', open:true},
+          { id:11, pId:1, name:'随意勾选 1-1', open:true},
+          { id:111, pId:11, name:'随意勾选 1-1-1'},
+          { id:112, pId:11, name:'随意勾选 1-1-2'},
+          { id:12, pId:1, name:'随意勾选 1-2', open:true},
+          { id:121, pId:12, name:'随意勾选 1-2-1'},
+          { id:122, pId:12, name:'随意勾选 1-2-2'},
+          { id:2, pId:0, name:'随意勾选 2', checked:true, open:true},
+          { id:21, pId:2, name:'随意勾选 2-1'},
+          { id:22, pId:2, name:'随意勾选 2-2', open:true},
+          { id:221, pId:22, name:'随意勾选 2-2-1', checked:true},
+          { id:222, pId:22, name:'随意勾选 2-2-2'},
+          { id:23, pId:2, name:'随意勾选 2-3'}
+        ],
 
+      }
+    };
     
+    setTrees();
+    function setTrees(){                                          // 树结构
+      var setting = {
+        check: {
+          enable: true
+        },
+        data: {
+          simpleData: {
+            enable: true
+          }
+        }
+      };
+      $('.js-trees-ele').each(function(){
+        var $self=$(this);
+        var key=$self.attr('data-key') || '';
+        if(key!=''){
+          $.fn.zTree.init($self, setting, def.trees[key]);
+        }
+      });
+    }
   })
 
        

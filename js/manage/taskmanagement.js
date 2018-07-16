751e190d4fc4119077e00141ddf93299ed7850df
vian127 <vian127@163.com>
2018-07-16 18:18:38 +0800

    管理列表页筛选功能

--- js/manage/taskmanagement.js
+++ js/manage/taskmanagement.js
@@ -1,12 +1,8 @@
-require.config({
-    paths: {
-
-    },
-
-    shim: {
-       
-    }
-});
+/*
+    #author     gaofeng
+    #date       2017/07/16
+    #purpose    管理列表页
+*/
 
 require(['vue', 'msg', 'layui'], function (Vue, msg) {
     $(function () {
@@ -15,26 +11,26 @@ require(['vue', 'msg', 'layui'], function (Vue, msg) {
             data() {
               return  infoAll={
                     taskTh:[
-                        {id:1,is_show:true,name:'任务标题'},
-                        {id:1,is_show:true,name:'任务类型'},
-                        {id:1,is_show:true,name:'问卷'},
-                        {id:1,is_show:true,name:'任务客户数'},
-                        {id:1,is_show:true,name:'已完成数量'},
-                        {id:1,is_show:true,name:'执行中数量'},
-                        {id:1,is_show:true,name:'待指派数量'},
-                        {id:1,is_show:true,name:'创建时间'},
-                        {id:1,is_show:true,name:'任务期限'},
-                        {id:1,is_show:true,name:'创建人'},
+                        {id:3,is_show:true,name:'任务标题'},
+                        {id:4,is_show:true,name:'任务类型'},
+                        {id:5,is_show:true,name:'问卷'},
+                        {id:6,is_show:true,name:'任务客户数'},
+                        {id:7,is_show:true,name:'已完成数量'},
+                        {id:8,is_show:true,name:'执行中数量'},
+                        {id:9,is_show:true,name:'待指派数量'},
+                        {id:10,is_show:true,name:'创建时间'},
+                        {id:11,is_show:true,name:'任务期限'},
+                        {id:12,is_show:true,name:'创建人'},
                     ],
                     taskTb:[
-                        {id:1,is_show:true,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
-                        {id:2,is_show:true,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
-                        {id:3,is_show:true,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
-                        {id:4,is_show:true,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
-                        {id:5,is_show:true,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
-                        {id:6,is_show:true,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
-                        {id:7,is_show:true,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
-                    ],is_show:true,
+                        {id:1,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
+                        {id:2,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
+                        {id:3,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
+                        {id:4,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
+                        {id:5,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
+                        {id:6,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
+                        {id:7,title:'新客户',type:'调查问卷',roll:'内容',clientNum:'5',successNum:'6',executory:'7',noAssigned:'8',created:'2018-6-12 12:12:12:00',limit:'2018-12-12 12:12:00',creater:'客服经理'},
+                    ],
                     taskFilter:{
                         title:'客服',
                         type:'1',
@@ -50,26 +46,27 @@ require(['vue', 'msg', 'layui'], function (Vue, msg) {
                 }
             },
             methods:{
-                toggleSelect:function(){
+                toggleSelect:function(){                                                     //显示隐藏
                     this.list_info.is_show_select=!this.list_info.is_show_select;
                 },
-                changeCol:function(event,id){
-                    var tag = event.target;
+                changeCol:function(event,id){                                               //单选
+                    var tag=event.target;
                     var is_check=tag.checked;
                     if(is_check==true){
                         $('.js-list-table').find('.js-col'+id+'-ele').show();
                     }else{
-                        $('.js-list-table').find('.js-col'+id+'-ele').hide()
+                        $('.js-list-table').find('.js-col'+id+'-ele').hide();
                     }
                     var checked_len=0;
                     this.taskTh.forEach(function(item){
                         if(id==item.id){
                             item.is_show=is_check;
                         }
-                        item.is_show=true?checked_len++:checked_len;
+                        item.is_show==true?checked_len++:checked_len;
                     });
-                     // 全选判断
-                     if(is_check==true){
+
+                    // 全选判断
+                    if(is_check==true){
                         if(this.list_info.checked_all==false && checked_len==this.taskTh.length){
                             this.list_info.checked_all=true;
                         }
@@ -89,7 +86,14 @@ require(['vue', 'msg', 'layui'], function (Vue, msg) {
                         $('.js-list-table').find('.js-can-hide').hide();
                         this.taskTh.forEach(item=>item.is_show=false);
                     }
+                },
+                saveSelect:function(){                                                      //确认提交选择
+                    console.log(this.thead);
+                    this.toggleSelect();
+                },
 
+                filterFunc:function(){                                                      //确认筛选列表
+                    console.log(this.collect);
                 },
                 page:function(){
                     layui.use('laypage', function () {
@@ -112,8 +116,6 @@ require(['vue', 'msg', 'layui'], function (Vue, msg) {
             }
         })
        
-        $('.js-filter').on('click',function(){
-            console.log(vm.taskFilter)
-        })
+       
     })
 })

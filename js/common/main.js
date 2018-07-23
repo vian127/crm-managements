22750ec72cc4678b18d15f8b6d42354c9386d7fd
Binlu <826202368@qq.com>
2018-07-23 09:36:12 +0800

    开发计划

--- js/common/main.js
+++ js/common/main.js
@@ -6,7 +6,6 @@
 require.config({
   // baseUrl: "file:///E:/myWorks/github/cooperation/crm-managements/js",
   baseUrl: "file:///E:/project/works/crm-product/js",
-  // baseUrl: " file:///E:/crm-managements/js",
   urlArgs: "r=" + (new Date()).getTime(),
   // urlArgs:"t="+static_change_time,
   paths: {
